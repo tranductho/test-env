0dca5d62c7fc815a2d37cfb475a29219a8c0fb4a
Tho Tran <tranductho@gmail.com>
2025-12-17 11:03:45 +0700

    init nodejs app

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    env: process.env.NODE_ENV,
+    message: "Hello from " + process.env.NODE_ENV
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
